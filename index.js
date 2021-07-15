76c4ee6a0583e09d7520bdde0c947f2faf5a451c
Kai Sawamoto <kaisawamoto@gmail.com>
2021-07-15 13:46:47 -0700

    twillio

--- index.js
+++ index.js
@@ -86,11 +86,3 @@ const client = require("twilio")(accountSid, authToken);
     }
   }
 })();
-
-client.messages
-  .create({
-    body: "This is the ship that made the Kessel Run in fourteen parsecs?",
-    from: "+15017122661",
-    to: "+15558675310",
-  })
-  .then((message) => console.log(message.sid));
